2f31154ac204ac8002faaa459756c8bb0044a3e2
Michael OBrien <obrien.michael.t@gmail.com>
2026-02-15 10:35:58 -0500

    Creates client.ts file

---
+++ netlify/db/client.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.NETLIFY_DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+export const db = drizzle(pool);
