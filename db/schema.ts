81827687fe778c0feb5dc2a32306f6e6e75787dc
Michael OBrien <obrien.michael.t@gmail.com>
2026-02-16 08:24:32 -0500

    sets up the bp db schema

--- db/schema.ts
+++ db/schema.ts
@@ -12,3 +12,17 @@ export const users = pgTable('users', {
   password_hash: varchar('password_hash', { length: 255 }).notNull(),
   created_at: timestamp('created_at').defaultNow(),
 });
+
+export const bloodpressure = pgTable('bloodpressure', {
+  id: serial('id').primaryKey(),
+  user_id: integer('user_id')
+    .notNull()
+    .references(() => users.id),
+  systolic1: integer('systolic1').notNull(),
+  diastolic1: integer('diastolic1').notNull(),
+  heart_rate1: integer('heart_rate1').notNull(),
+  systolic2: integer('systolic2').notNull(),
+  diastolic2: integer('diastolic2').notNull(),
+  heart_rate2: integer('heart_rate2').notNull(),
+  created_at: timestamp('created_at').defaultNow(),
+})
