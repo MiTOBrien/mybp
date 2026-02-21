e937f58b903632df9cb39bc74b93017ac7cd8b3e
Michael OBrien <obrien.michael.t@gmail.com>
2026-02-21 10:12:19 -0500

    updates client.js file for netlify serverless in prod

--- netlify/db/client.js
+++ netlify/db/client.js
@@ -1,5 +1,6 @@
 import { neon } from "@netlify/neon";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/neon-serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
 
 const sql = neon(process.env.NETLIFY_DATABASE_URL);
 export const db = drizzle(sql);
